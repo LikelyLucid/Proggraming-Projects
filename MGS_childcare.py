e018b9706bc82a6397209e08230f517ad506333f
PurelyStupidity <micoolplays@gmail.com>
2022-02-22 13:57:51 +1300

    February 22, 2022 1:57 PM

---
+++ MGS_childcare.py
@@ -0,0 +1,22 @@
+choice = O
+while choice 5:
+print("Welcome to MGS Childcare")
+would you like to do? Please choose one of the items
+print("
+      print()
+      print("l Drop off a child")
+      print("2 Pick up a child")
+      print("3 Calculate cost")
+      print("4 Print roll")
+      print("5 Exit the system")
+      print()
+      your choice(number between 1 and 5): "))
+    print()
+    if:
+    dropOff()
+    elif choice == 2:
+    picklJp()
+    elif choice == 3:
+    calcCost()
+    elif
+    printRoll()
