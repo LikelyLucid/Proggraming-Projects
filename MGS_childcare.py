9c619682244c1efe23c3ef0b155754afb64a1454
PurelyStupidity <micoolplays@gmail.com>
2022-02-22 14:04:39 +1300

    February 22, 2022 2:04 PM

--- MGS_childcare.py
+++ MGS_childcare.py
@@ -1,7 +1,8 @@
 """
 You are to design and write a program that will be used by a child day-care centre. It will keep track of children throughout the day. There will be many features in the program, but we will take it a step at a time and build each one in turn."""
 
-def dropoff
+def dropOff():
+    
 
 
 
