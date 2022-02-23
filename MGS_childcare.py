79e1737d9d4bbc4db03c3e1c7e6decb04f3e0964
PurelyStupidity <micoolplays@gmail.com>
2022-02-24 09:52:56 +1300

    February 24, 2022 9:52 AM

--- MGS_childcare.py
+++ MGS_childcare.py
@@ -1,6 +1,6 @@
 """
 You are to design and write a program that will be used by a child day-care centre. It will keep track of children throughout the day. There will be many features in the program, but we will take it a step at a time and build each one in turn."""
-child
+children = []
 
 choice = 0
 while choice != 5:
