6a16ce9953a26d18a049e0ad39b8904fcbb30f9a
PurelyStupidity <micoolplays@gmail.com>
2022-02-24 10:13:36 +1300

    February 24, 2022 10:13 AM

--- MGS_childcare.py
+++ MGS_childcare.py
@@ -21,7 +21,7 @@ def pickUp():
     try:
         name = input("Name: ").capitalize()
         children.remove(name)
-        print(f"{name} has been removed")
+        print(f"{name} has been removed from roll")
     except ValueError:
         print("Child not found")
 
