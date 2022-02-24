c2b29b3818e6f10f3fef5a12d4d9c0f27cfa068a
PurelyStupidity <micoolplays@gmail.com>
2022-02-25 11:40:02 +1300

    February 25, 2022 11:40 AM

--- taxi_trips.py
+++ taxi_trips.py
@@ -1,20 +1,4 @@
-"""
-The program should start off by asking for the driver’s name. It should then repeatedly ask to start a new trip.
 
-If the user says "yes", ask for the time the trip took in minutes. It should print out the cost at the end of the trip which is a base cost of $10 plus $2 extra for each minute the trip took.
-
-At the end of the day the user will say "no" to more trips, and the system should print:
-
-● The driver’s name
-
-● Total time of all trips
-
-● Average time of all trips
-
-● Total income for the day
-
-● Average cost of all trip
-"""
 name = input("What is your name? ")
 total_time = 0
 average_time = []
