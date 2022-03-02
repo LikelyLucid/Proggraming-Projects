2deb6267154194af178d632ed82ec1f565e8ae50
PurelyStupidity <micoolplays@gmail.com>
2022-03-03 10:19:35 +1300

    March 3, 2022 10:19 AM

--- absenses_at_work.py
+++ absenses_at_work.py
@@ -6,17 +6,6 @@ while True:
         break
     absent_days = int(input("Enter number of absent days: "))
     employees[name] = absent_days
-
-"""
-The program now will print this
-1. The average number of days of absence per year
-
-2. The name of the person who had the most days of absence
-
-3. An alphabetical list of the names of the people who were not absent at all during the year
-
-4. The names of the people whose period of absence was above the average for the year. (This could be an alphabetical list or a list sorted in descending order of days of absence)"""
-
 average_days_off = sum(employees.values()) / len(employees)
 print("The average number of days of absence per year is: ", average_days_off)
 
