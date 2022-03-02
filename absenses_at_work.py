6e32a9ef65bf32bbdf492d8cc5364c877b0424bd
PurelyStupidity <micoolplays@gmail.com>
2022-03-03 10:17:51 +1300

    March 3, 2022 10:17 AM

--- absenses_at_work.py
+++ absenses_at_work.py
@@ -7,8 +7,11 @@ while True:
     absent_days = int(input("Enter number of absent days: "))
     employees[name] = absent_days
 
-#sort employees by number of absent days
-sorted_employees = sorted(employees.items(), key=lambda x: x[1])
+"""
+1. The average number of days of absence per year
 
-for employee in sorted_employees:
-    print(employee[0], employee[1])
+2. The name of the person who had the most days of absence
+
+3. An alphabetical list of the names of the people who were not absent at all during the year
+
+4. The names of the people whose period of absence was above the average for the year. (This could be an alphabetical list or a list sorted in descending order of days of absence)"""
