1dc19696a45bcfe5e8b246ca054bd3a26cc29c7e
PurelyStupidity <micoolplays@gmail.com>
2022-03-03 10:18:21 +1300

    March 3, 2022 10:18 AM

--- absenses_at_work.py
+++ absenses_at_work.py
@@ -16,3 +16,5 @@ The program now will print this
 3. An alphabetical list of the names of the people who were not absent at all during the year
 
 4. The names of the people whose period of absence was above the average for the year. (This could be an alphabetical list or a list sorted in descending order of days of absence)"""
+
+ave
