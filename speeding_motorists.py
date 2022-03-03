194cab2cc2edbbaafabe0ae4e1c7cb16d0a1a8c9
PurelyStupidity <micoolplays@gmail.com>
2022-03-04 11:37:40 +1300

    March 4, 2022 11:37 AM

--- speeding_motorists.py
+++ speeding_motorists.py
@@ -26,7 +26,7 @@ def interger_checker(question):
         print("Please enter an integer")
         return interger_checker(question)
 while True:
-    print("")
+    print("############################################")
     name = input("enter the drivers full name: ").capitalize()
 
     if name in criminals:
