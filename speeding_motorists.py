e11fa787e3fa5afe67844ab334b6b846dac8185a
PurelyStupidity <micoolplays@gmail.com>
2022-03-04 11:26:58 +1300

    March 4, 2022 11:26 AM

--- speeding_motorists.py
+++ speeding_motorists.py
@@ -18,7 +18,7 @@ fines:
 
 >= 45 km/h = $630
 """
-criminals = ["James Wilson", "Helga Norman", ""]
+criminals = ["James Wilson", "Helga Norman", "Zachary Conroy"]
 
 def check_total_speed(speed):
     if speed < 10:
